79a9fb04c8a1bfc49bb29d7c181adae0ac80a354
SuoRuGithub <z-z23@mails.tsinghua.edu.cn>
2024-07-29 23:07:11 +0800

    test

---
+++ build/CMakeFiles/Construct_3D_World.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/zzpku/myCode/OOP/Construct3DWorld/Controller/ControllerBase.cpp" "/home/zzpku/myCode/OOP/Construct3DWorld/build/CMakeFiles/Construct_3D_World.dir/Controller/ControllerBase.cpp.o"
+  "/home/zzpku/myCode/OOP/Construct3DWorld/Controller/ControllerCLI.cpp" "/home/zzpku/myCode/OOP/Construct3DWorld/build/CMakeFiles/Construct_3D_World.dir/Controller/ControllerCLI.cpp.o"
+  "/home/zzpku/myCode/OOP/Construct3DWorld/Model/Face3D.cpp" "/home/zzpku/myCode/OOP/Construct3DWorld/build/CMakeFiles/Construct_3D_World.dir/Model/Face3D.cpp.o"
+  "/home/zzpku/myCode/OOP/Construct3DWorld/Model/Line3D.cpp" "/home/zzpku/myCode/OOP/Construct3DWorld/build/CMakeFiles/Construct_3D_World.dir/Model/Line3D.cpp.o"
+  "/home/zzpku/myCode/OOP/Construct3DWorld/Model/ModelBase.cpp" "/home/zzpku/myCode/OOP/Construct3DWorld/build/CMakeFiles/Construct_3D_World.dir/Model/ModelBase.cpp.o"
+  "/home/zzpku/myCode/OOP/Construct3DWorld/Model/Point3D.cpp" "/home/zzpku/myCode/OOP/Construct3DWorld/build/CMakeFiles/Construct_3D_World.dir/Model/Point3D.cpp.o"
+  "/home/zzpku/myCode/OOP/Construct3DWorld/Model/objModel.cpp" "/home/zzpku/myCode/OOP/Construct3DWorld/build/CMakeFiles/Construct_3D_World.dir/Model/objModel.cpp.o"
+  "/home/zzpku/myCode/OOP/Construct3DWorld/View/View.cpp" "/home/zzpku/myCode/OOP/Construct3DWorld/build/CMakeFiles/Construct_3D_World.dir/View/View.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/Construct_3D_World.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Construct_3D_World.dir/Controller/ControllerBase.cpp.o"
+  "CMakeFiles/Construct_3D_World.dir/Controller/ControllerCLI.cpp.o"
+  "CMakeFiles/Construct_3D_World.dir/Model/Face3D.cpp.o"
+  "CMakeFiles/Construct_3D_World.dir/Model/Line3D.cpp.o"
+  "CMakeFiles/Construct_3D_World.dir/Model/ModelBase.cpp.o"
+  "CMakeFiles/Construct_3D_World.dir/Model/Point3D.cpp.o"
+  "CMakeFiles/Construct_3D_World.dir/Model/objModel.cpp.o"
+  "CMakeFiles/Construct_3D_World.dir/View/View.cpp.o"
+  "Construct_3D_World"
+  "Construct_3D_World.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Construct_3D_World.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
